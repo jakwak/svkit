ec8d21105013193b7165eb3a0b32f0159e5e6b62
Ondalssam <ondalssam@gmail.com>
2025-03-24 16:18:57 +0900

    ...

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,3 +4,12 @@ export const load: LayoutServerLoad = async ( { fetch} ) => {
   const res = await fetch('/api/me').then(res => res.json());
   return {cur_user: res.username};
 };
+
+
+// export async function logout() {
+//   await fetch('/api/logout', {
+//       method: 'POST',
+//       credentials: 'include'
+//   });
+//   window.location.href = '/';
+// }
