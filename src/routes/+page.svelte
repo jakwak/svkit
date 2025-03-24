a1f23994f269e2f06eaad6978227308121a958e0
Kwak Jaehak <ondalssam@gmail.com>
2025-03-24 22:07:43 +0900

    ...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,38 @@
 <script lang="ts">
+  import { onDestroy, onMount } from 'svelte'
 	import type { PageProps } from './$types';
   
 	let { data }: PageProps = $props();
+
+	let socket: WebSocket
+	let status = $state("Disconnected");
+
+	let cur_user = data.cur_user ? data.cur_user : "Guest";
+
+	onMount(() => {
+		socket = new WebSocket('ws://localhost:8000/ws/' + cur_user)
+
+		socket.onopen = () => {
+			status = "Connected"
+		}
+		
+		socket.onmessage = (event) => {
+			status = event.data
+		}
+
+		socket.onclose = () => {
+			status = "Disconnected"
+		}
+	})
+
+	onDestroy(() => {
+		if (socket) {
+			socket.close()
+		}
+	})
 </script>
 
 <h1>Home ===> {data.cur_user}</h1>
 <p>data: {JSON.stringify(data)}</p>
+
+<h1 class="text-2xl text-blue-500">{status}</h1>
