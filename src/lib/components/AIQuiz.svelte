bf2d42b10cc72c5cf9b59c2d78a20aaa276b8250
Ondalssam <ondalssam@gmail.com>
2025-04-09 16:23:00 +0900

    ...

--- src/lib/components/AIQuiz.svelte
+++ src/lib/components/AIQuiz.svelte
@@ -33,7 +33,7 @@
       />
       {#if topic}
       <button
-        type="submit"
+        type="button"
         class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600"
         aria-label="Close"
         onclick={() => (topic = '')}
